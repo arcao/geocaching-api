c6f5048da75aadf1e47a503b74eeee1d9c3753eb
Arcao <arcao@arcao.com>
2012-07-08 20:59:35 +0200

    Changed testing licence key

--- src/test/java/com/arcao/geocaching/api/live_geocaching_api/AbstractGeocachingTest.java
+++ src/test/java/com/arcao/geocaching/api/live_geocaching_api/AbstractGeocachingTest.java
@@ -10,7 +10,7 @@ public abstract class AbstractGeocachingTest {
   
   // Please do not use this keys in production. It's only for testing!!!
   protected static final String CONSUMER_KEY = "90C7F340-7998-477D-B4D3-AC48A9A0F560";
-  protected static final String LICENCE_KEY = "40940392-0C8E-487B-BC40-EA250D6D9AE0";
+  protected static final String LICENCE_KEY = "55BBC3C1-24CF-4D1B-B7EC-7A8E75DAB7D1";//"40940392-0C8E-487B-BC40-EA250D6D9AE0";
   
   protected static final String ANONYMOUS_USER = "";
   protected static final String ANONYMOUS_PASSWORD = "";
