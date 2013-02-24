a4bceac81ae3c1a33cfa742cb5fb0d368090f9ce
Arcao <arcao@arcao.com>
2013-02-24 15:56:24 +0100

    Updated JUnit test environment for Geocaching API stagging server

--- src/test/java/com/arcao/geocaching/api/live_geocaching_api/AbstractGeocachingTest.java
+++ src/test/java/com/arcao/geocaching/api/live_geocaching_api/AbstractGeocachingTest.java
@@ -9,12 +9,16 @@ public abstract class AbstractGeocachingTest {
   protected static GeocachingApi api = null;
   
   // Please do not use this in production. It's only for testing!!!
-  protected static final String TEST_USER = "geocaching-api";
-  protected static final String TEST_AUTH_TOKEN = "u5HIMAyRXz1MsZ5ylHHzOjkkuFs="; // generated via oAuth example, see: https://github.com/arcao/geocaching-api-examples
+  protected static final String TEST_USER = "gc-java-api-premium";
+  // generated via oAuth example, see: https://github.com/arcao/geocaching-api-examples
+  protected static final String TEST_AUTH_TOKEN = "s0oLcAnxgA/KV78b2/R7PamqBWc=";
+  
+  // Geocaching API service URL for test environment mode
+  protected static final String STAGGING_SERVICE_URL = "https://staging.api.groundspeak.com/Live/v6beta/geocaching.svc"; 
 
   @BeforeClass
   public static void setUp() throws Exception {
-    api = new LiveGeocachingApi();
+    api = new LiveGeocachingApi(STAGGING_SERVICE_URL);
     
     api.openSession(TEST_AUTH_TOKEN);
   }
