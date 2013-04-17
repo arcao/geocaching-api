14861dc2ac5397597b88c4e5d6b741a3f4454a42
Arcao <arcao@arcao.com>
2013-04-17 08:58:33 +0200

    Improved error reporting
    Stack trace now contains also server side exception details

--- src/main/java/com/arcao/geocaching/api/impl/live_geocaching_api/exception/LiveGeocachingApiException.java
+++ src/main/java/com/arcao/geocaching/api/impl/live_geocaching_api/exception/LiveGeocachingApiException.java
@@ -5,35 +5,58 @@ import com.arcao.geocaching.api.impl.live_geocaching_api.StatusCode;
 import com.arcao.geocaching.api.impl.live_geocaching_api.parser.StatusJsonParser.Status;
 
 public class LiveGeocachingApiException extends GeocachingApiException {
-  private static final long serialVersionUID = 4459503722935685091L;
-  
-  protected StatusCode statusCode;
-  protected int originalStatusCode;
-  protected String exceptionDetails;
-  
-  public LiveGeocachingApiException(Status status) {
-    super(status.getStatusMessage());
+	private static final long serialVersionUID = 4459503722935685091L;
 
-    statusCode = status.getStatusCode();
-    originalStatusCode = status.getOriginalStatusCode();
-    exceptionDetails = status.getExceptionDetails();
-  }
-  
-  public StatusCode getStatusCode() {
-    return statusCode;
-  }
-  
-  public int getOriginalStatusCode() {
-    return originalStatusCode;
-  }
-  
-  public String getExceptionDetails() {
-    return exceptionDetails;
-  }
-  
-  @Override
-  public String toString() {
-    return super.toString() + " (" + originalStatusCode + ")";
-  }
+	protected StatusCode statusCode;
+	protected int originalStatusCode;
+	protected String exceptionDetails;
 
+	public LiveGeocachingApiException(Status status) {
+		super(status.getStatusMessage());
+
+		statusCode = status.getStatusCode();
+		originalStatusCode = status.getOriginalStatusCode();
+		exceptionDetails = status.getExceptionDetails();
+
+		if (exceptionDetails != null && exceptionDetails.length() > 0) {
+			initCause(new LiveGeocachingApiRemoteException(exceptionDetails));
+		}
+	}
+
+	public StatusCode getStatusCode() {
+		return statusCode;
+	}
+
+	public int getOriginalStatusCode() {
+		return originalStatusCode;
+	}
+
+	public String getExceptionDetails() {
+		return exceptionDetails;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(super.toString());
+		sb.append(" (").append(originalStatusCode).append(")");
+
+		return sb.toString();
+	}
+
+	private static class LiveGeocachingApiRemoteException extends Exception {
+		private static final long serialVersionUID = 6441674083027722835L;
+
+		private final String exceptionDetails;
+
+		public LiveGeocachingApiRemoteException(String exceptionDetails) {
+			this.exceptionDetails = exceptionDetails;
+		}
+
+		@Override
+		public String toString() {
+			return exceptionDetails;
+		}
+	}
 }
