a3c7e9f7a3a2363670bc3d4023ecff50601eaccc
Arcao <arcao@arcao.com>
2013-08-27 08:10:09 +0200

    Used 60s for connection timeout
    This should fix the JUnit tests which failed on timeout.

--- src/main/java/com/arcao/geocaching/api/configuration/impl/AbstractGeocachingApiConfiguration.java
+++ src/main/java/com/arcao/geocaching/api/configuration/impl/AbstractGeocachingApiConfiguration.java
@@ -3,7 +3,7 @@ package com.arcao.geocaching.api.configuration.impl;
 import com.arcao.geocaching.api.configuration.GeocachingApiConfiguration;
 
 public abstract class AbstractGeocachingApiConfiguration implements GeocachingApiConfiguration {
-	protected static final int DEFAULT_TIMEOUT = 30000; 
+	protected static final int DEFAULT_TIMEOUT = 60000; 
 	
 	public int getConnectTimeout() {
 		return DEFAULT_TIMEOUT;
