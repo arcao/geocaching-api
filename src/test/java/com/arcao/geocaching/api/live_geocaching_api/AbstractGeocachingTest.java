78d71cdf380b3e22964e1be303cbb58cdbd960ec
Arcao <arcao@arcao.com>
2014-04-06 14:52:34 +0200

    Updated access token for JUnit testa

--- src/test/java/com/arcao/geocaching/api/live_geocaching_api/AbstractGeocachingTest.java
+++ src/test/java/com/arcao/geocaching/api/live_geocaching_api/AbstractGeocachingTest.java
@@ -8,16 +8,16 @@ import com.arcao.geocaching.api.impl.LiveGeocachingApi;
 
 public abstract class AbstractGeocachingTest {
   protected static GeocachingApi api = null;
-  
+
   // Please do not use this in production. It's only for testing!!!
   protected static final String TEST_USER = "APIUser_508689241";
   // generated via oAuth example, see: https://github.com/arcao/geocaching-api-examples
-  protected static final String TEST_AUTH_TOKEN = "9SCa1ii9jKsCL22h9+HUWo+dVII=";
-  
+  protected static final String TEST_AUTH_TOKEN = "wzWIiStBEkH2Ql8pyMlZw5yEqms=";
+
   @BeforeClass
   public static void setUp() throws Exception {
     api = new LiveGeocachingApi(new DefaultStaggingGeocachingApiConfiguration());
-    
+
     api.openSession(TEST_AUTH_TOKEN);
   }
 }
