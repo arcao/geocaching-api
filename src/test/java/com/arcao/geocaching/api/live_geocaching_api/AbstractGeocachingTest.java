9357cc736db06b361e14e283885775e75e44b680
Arcao <arcao@arcao.com>
2014-04-15 08:12:14 +0200

    Updated testing access token.

--- src/test/java/com/arcao/geocaching/api/live_geocaching_api/AbstractGeocachingTest.java
+++ src/test/java/com/arcao/geocaching/api/live_geocaching_api/AbstractGeocachingTest.java
@@ -9,10 +9,8 @@ import com.arcao.geocaching.api.impl.LiveGeocachingApi;
 public abstract class AbstractGeocachingTest {
   protected static GeocachingApi api = null;
 
-  // Please do not use this in production. It's only for testing!!!
-  protected static final String TEST_USER = "APIUser_508689241";
   // generated via oAuth example, see: https://github.com/arcao/geocaching-api-examples
-  protected static final String TEST_AUTH_TOKEN = "wzWIiStBEkH2Ql8pyMlZw5yEqms=";
+  protected static final String TEST_AUTH_TOKEN = "hFHmU3c7wnljwzCg4QodthxIuMo=";
 
   @BeforeClass
   public static void setUp() throws Exception {
