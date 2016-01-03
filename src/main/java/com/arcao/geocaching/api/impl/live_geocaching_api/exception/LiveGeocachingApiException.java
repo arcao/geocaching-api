633bfb86f12f85d1162faebf74c071c25409ec1d
Arcao <arcao@arcao.com>
2016-01-03 22:02:37 +0100

    Detailed exception message from service is now part of LiveGeocachingApiException message (for better logging)

--- src/main/java/com/arcao/geocaching/api/impl/live_geocaching_api/exception/LiveGeocachingApiException.java
+++ src/main/java/com/arcao/geocaching/api/impl/live_geocaching_api/exception/LiveGeocachingApiException.java
@@ -7,51 +7,60 @@ import com.arcao.geocaching.api.impl.live_geocaching_api.Status;
 public class LiveGeocachingApiException extends GeocachingApiException {
 	private static final long serialVersionUID = 4459503722935685091L;
 
+	protected final String originalMessage;
 	protected final StatusCode statusCode;
-	protected final int originalStatusCode;
+	protected final int code;
 	protected final String exceptionDetails;
 
 	public LiveGeocachingApiException(Status status) {
 		super(status.getMessage());
+		initCause(null);  // Disallow subsequent initCause
 
+		originalMessage = status.getMessage();
 		statusCode = status.getStatusCode();
-		originalStatusCode = status.getCode();
+		code = status.getCode();
 		exceptionDetails = status.getExceptionDetails();
+	}
 
-		if (exceptionDetails != null && exceptionDetails.length() > 0) {
-			initCause(new LiveGeocachingApiRemoteException(exceptionDetails));
+	@Override
+	public String getMessage() {
+		if (exceptionDetails == null || exceptionDetails.length() == 0) {
+			return super.getMessage() + " (" + code + ")";
+		} else {
+			return super.getMessage() + " (" + code + "); nested exception is: \n\t" +
+							exceptionDetails;
 		}
 	}
 
+	/**
+	 * Returns unmodified error message.
+	 * @return unmodified error message
+   */
+	public String getOriginalMessage() {
+		return originalMessage;
+	}
+
+	/**
+	 * Returns error code represented by StatusCode enum.
+	 * @return error code
+   */
 	public StatusCode getStatusCode() {
 		return statusCode;
 	}
 
-	public int getOriginalStatusCode() {
-		return originalStatusCode;
+	/**
+	 * Returns error code as an integer.
+	 * @return error code
+   */
+	public int getCode() {
+		return code;
 	}
 
+	/**
+	 * Returns exception details received from Live Geocaching service.
+	 * @return exception details
+   */
 	public String getExceptionDetails() {
 		return exceptionDetails;
-	}
-
-	@Override
-	public String toString() {
-        return super.toString() + " (" + originalStatusCode + ")";
-	}
-
-	private static class LiveGeocachingApiRemoteException extends Exception {
-		private static final long serialVersionUID = 6441674083027722835L;
-
-		private final String exceptionDetails;
-
-		public LiveGeocachingApiRemoteException(String exceptionDetails) {
-			this.exceptionDetails = exceptionDetails;
-		}
-
-		@Override
-		public String toString() {
-			return exceptionDetails;
-		}
 	}
 }
