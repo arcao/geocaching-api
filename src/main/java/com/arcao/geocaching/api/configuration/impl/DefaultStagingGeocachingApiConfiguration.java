8a4973fc88fd08a71e085bf17980b109ed687272
Arcao <arcao@arcao.com>
2016-01-03 21:58:25 +0100

    Fixed typo error

--- src/main/java/com/arcao/geocaching/api/configuration/impl/DefaultStagingGeocachingApiConfiguration.java
+++ src/main/java/com/arcao/geocaching/api/configuration/impl/DefaultStagingGeocachingApiConfiguration.java
@@ -1,9 +1,9 @@
 package com.arcao.geocaching.api.configuration.impl;
 
 public class DefaultStagingGeocachingApiConfiguration extends AbstractGeocachingApiConfiguration {
-	protected static final String STAGGING_ENTRY_POINT_URL = "https://staging.api.groundspeak.com/Live/v6beta/geocaching.svc";
+	protected static final String STAGING_ENTRY_POINT_URL = "https://staging.api.groundspeak.com/Live/v6beta/geocaching.svc";
 
 	public String getApiServiceEntryPointUrl() {
-		return STAGGING_ENTRY_POINT_URL;
+		return STAGING_ENTRY_POINT_URL;
 	}
 }
