719d5218dcf7049a20ddbfa17804b1609926f981
Arcao <arcao@arcao.com>
2016-01-06 21:19:39 +0100

    Added new fields to GeocacheExclusionsFilter

--- src/main/java/com/arcao/geocaching/api/impl/live_geocaching_api/filter/GeocacheExclusionsFilter.java
+++ src/main/java/com/arcao/geocaching/api/impl/live_geocaching_api/filter/GeocacheExclusionsFilter.java
@@ -9,16 +9,27 @@ public class GeocacheExclusionsFilter implements Filter {
 	
 	private final Boolean archived;
 	private final Boolean available;
+	private final Boolean hasCorrectedCoordinates;
+	private final Boolean hasPersonalCacheNote;
 	private final Boolean premium;
+	private final Boolean published;
 	
 	public GeocacheExclusionsFilter(Boolean archived, Boolean available, Boolean premium) {
+		this(archived, available, null, null, premium, null);
+	}
+
+	public GeocacheExclusionsFilter(Boolean archived, Boolean available, Boolean hasCorrectedCoordinates, Boolean hasPersonalCacheNote, Boolean premium, Boolean published) {
 		this.archived = archived;
 		this.available = available;
+		this.hasCorrectedCoordinates = hasCorrectedCoordinates;
+		this.hasPersonalCacheNote = hasPersonalCacheNote;
 		this.premium = premium;
+		this.published = published;
 	}
-	
+
+
 	public boolean isValid() {
-		return archived != null || available != null || premium != null;
+		return archived != null || available != null || hasCorrectedCoordinates != null || hasPersonalCacheNote != null || premium != null || published != null;
 	}
 	
 	public void writeJson(JsonWriter w) throws IOException {
@@ -28,8 +39,14 @@ public class GeocacheExclusionsFilter implements Filter {
 			w.name("Archived").value(archived);
 		if (available != null)
 			w.name("Available").value(available);
+		if (hasCorrectedCoordinates != null)
+			w.name("HasCorrectedCoordinates").value(hasCorrectedCoordinates);
+		if (hasPersonalCacheNote != null)
+			w.name("HasPersonalCacheNote").value(hasPersonalCacheNote);
 		if (premium != null)
 			w.name("Premium").value(premium);
+		if (published != null)
+			w.name("Published").value(published);
 		w.endObject();
 	}
 
