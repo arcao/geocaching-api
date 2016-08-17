b69473a46bf338f25f37a1cad6b8b54ca025efb4
Arcao <arcao@arcao.com>
2016-08-17 10:44:49 +0200

    Fixed simpleGetUsersTrackablesTest

--- src/test/java/com/arcao/geocaching/api/live_geocaching_api/GetUsersTrackablesTest.java
+++ src/test/java/com/arcao/geocaching/api/live_geocaching_api/GetUsersTrackablesTest.java
@@ -1,17 +1,16 @@
 package com.arcao.geocaching.api.live_geocaching_api;
 
-import java.util.List;
-
+import com.arcao.geocaching.api.data.Trackable;
+import com.arcao.geocaching.api.exception.GeocachingApiException;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.arcao.geocaching.api.data.Trackable;
-import com.arcao.geocaching.api.exception.GeocachingApiException;
+import java.util.List;
 
 public class GetUsersTrackablesTest extends AbstractGeocachingTest {
 	@Test
 	public void simpleGetUsersTrackablesTest() throws GeocachingApiException {
 		List<Trackable> trackables = api.getUsersTrackables(0, 30, 0, false);
-		Assert.assertEquals(6, trackables.size()); // from staging geocaching.com website
+		Assert.assertEquals(7, trackables.size()); // from staging geocaching.com website
 	}
 }
