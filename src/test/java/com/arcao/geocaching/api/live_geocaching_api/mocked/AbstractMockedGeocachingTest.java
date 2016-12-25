4969a16ef1a5f31e0926e9220f24ec1c2498c1c9
Arcao <arcao@arcao.com>
2016-12-25 15:01:31 +0100

    Added a few tests using Mockito

---
+++ src/test/java/com/arcao/geocaching/api/live_geocaching_api/mocked/AbstractMockedGeocachingTest.java
@@ -0,0 +1,64 @@
+package com.arcao.geocaching.api.live_geocaching_api.mocked;
+
+import com.arcao.geocaching.api.GeocachingApi;
+import com.arcao.geocaching.api.LiveGeocachingApi;
+import com.arcao.geocaching.api.downloader.JsonDownloader;
+import com.arcao.geocaching.api.exception.GeocachingApiException;
+import com.arcao.geocaching.api.parser.JsonReader;
+
+import org.junit.Before;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+import static org.mockito.Mockito.mock;
+
+@SuppressWarnings("WeakerAccess")
+public abstract class AbstractMockedGeocachingTest {
+    private static final Charset CHARSET_UTF8 = Charset.forName("UTF-8");
+
+    GeocachingApi api = null;
+    JsonDownloader downloader = null;
+
+    private static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+
+    @Before
+    public void setUpMock() throws GeocachingApiException {
+        downloader = mock(JsonDownloader.class);
+
+        api = LiveGeocachingApi.builder()
+                .downloader(downloader)
+                .build();
+
+        api.openSession(ACCESS_TOKEN);
+    }
+
+    JsonReader createJsonReaderFromResource(String resourceFile) {
+        return new JsonReader(new InputStreamReader(getClass().getResourceAsStream(resourceFile), CHARSET_UTF8));
+    }
+
+    byte[] createByteArrayFromResource(String resourceFile) throws IOException {
+        InputStream inputStream = getClass().getResourceAsStream(resourceFile);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        byte[] buffer = new byte[4096];
+
+        try {
+            while (true) {
+                int len = inputStream.read(buffer);
+                if (len < 0)
+                    break;
+
+                outputStream.write(buffer, 0, len);
+            }
+        } finally {
+            inputStream.close();
+        }
+
+        return outputStream.toByteArray();
+
+    }
+}
---
+++ src/test/java/com/arcao/geocaching/api/live_geocaching_api/mocked/SearchForGeocachesMockedTest.java
@@ -0,0 +1,88 @@
+package com.arcao.geocaching.api.live_geocaching_api.mocked;
+
+import com.arcao.geocaching.api.GeocachingApi;
+import com.arcao.geocaching.api.data.Geocache;
+import com.arcao.geocaching.api.data.GeocacheLimits;
+import com.arcao.geocaching.api.data.type.ContainerType;
+import com.arcao.geocaching.api.data.type.GeocacheType;
+import com.arcao.geocaching.api.data.type.MemberType;
+import com.arcao.geocaching.api.exception.GeocachingApiException;
+import com.arcao.geocaching.api.parser.JsonReader;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class SearchForGeocachesMockedTest extends AbstractMockedGeocachingTest {
+    private final static String CACHE_CODE = "GCY81P";
+
+    @Test
+    public void getLiteGeocacheByCacheCodeNotFoundTest() throws GeocachingApiException {
+        JsonReader resultBody = createJsonReaderFromResource("result-lite-geocache-by-cache-code-not-found.json");
+
+        when(downloader.post(any(URL.class), any(byte[].class)))
+                .thenReturn(resultBody);
+
+        Geocache geocache = api.getGeocache(GeocachingApi.ResultQuality.LITE, CACHE_CODE, 0, 0);
+        assertNull(geocache);
+    }
+
+    @Test
+    public void getLiteGeocacheByCacheCodeRequestTest() throws GeocachingApiException, IOException {
+        byte[] expectedRequestBody = createByteArrayFromResource("request-lite-geocache-by-cache-code.json");
+        JsonReader resultBody = createJsonReaderFromResource("result-lite-geocache-by-cache-code.json");
+
+        when(downloader.post(any(URL.class), any(byte[].class)))
+                .thenReturn(resultBody);
+
+        Geocache geocache = api.getGeocache(GeocachingApi.ResultQuality.LITE, CACHE_CODE, 0, 0);
+
+        verify(downloader).post(any(URL.class), eq(expectedRequestBody));
+
+        // ResultQuality.LITE
+        assertNotNull(geocache);
+        assertNotNull(geocache.owner());
+        assertNotSame(MemberType.Guest, geocache.owner().memberType());
+        assertEquals(CACHE_CODE, geocache.code());
+        assertEquals(GeocacheType.Multi, geocache.geocacheType());
+        assertNotNull(geocache.placedBy());
+        assertEquals(ContainerType.Micro, geocache.containerType());
+        assertNotNull(geocache.createDate());
+        assertNotNull(geocache.placeDate());
+        assertNotNull(geocache.lastUpdateDate());
+        assertEquals(1.5F, geocache.difficulty(), 0);
+        assertEquals(1.5F, geocache.terrain(), 0);
+        assertFalse(Double.isNaN(geocache.coordinates().latitude()));
+        assertFalse(Double.isNaN(geocache.coordinates().longitude()));
+        assertNotNull(geocache.name());
+        assertFalse(geocache.archived());
+        assertTrue(geocache.available());
+        assertFalse(geocache.foundByUser());
+        assertFalse(geocache.premium());
+        assertNotSame(0, geocache.imageCount());
+        assertNotNull(geocache.url());
+        assertNotNull(geocache.guid());
+
+        GeocacheLimits limits = api.getLastGeocacheLimits();
+        assertNotNull(limits);
+        assertEquals(1, api.getLastSearchResultsFound());
+
+        GeocacheLimits geocacheLimits = api.getLastGeocacheLimits();
+        assertNotNull(geocacheLimits);
+        assertEquals(131, geocacheLimits.currentGeocacheCount());
+        assertEquals(9869, geocacheLimits.geocacheLeft());
+        assertEquals(10000, geocacheLimits.maxGeocacheCount());
+    }
+}
