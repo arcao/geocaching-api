f6c59fc89e6209ad8910d34b1dd52c7220a49c0b
Arcao <arcao@arcao.com>
2016-12-26 10:58:28 +0100

    Publish website only for tagged builds

--- .buildscript/push-site-to-gh-pages.sh
+++ .buildscript/push-site-to-gh-pages.sh
@@ -2,15 +2,15 @@
 
 SLUG="arcao/geocaching-api"
 JDK="oraclejdk8"
-BRANCH="master"
 
 set -e
 
-if [ "$TRAVIS_REPO_SLUG" == "$SLUG" ] && [ "$TRAVIS_JDK_VERSION" == "$JDK" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "$BRANCH" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "$SLUG" ] && [ "$TRAVIS_JDK_VERSION" == "$JDK" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   rm -rf $HOME/gh-pages/
   rm -rf $HOME/site-latest/
 
-  echo -e "Publishing site...\n"
+  echo -e "Publishing website...\n"
+  mvn clean site
 
   cp -R target/site $HOME/site-latest
 
@@ -23,9 +23,9 @@ if [ "$TRAVIS_REPO_SLUG" == "$SLUG" ] && [ "$TRAVIS_JDK_VERSION" == "$JDK" ] && 
   git rm -rf ./
   cp -Rf $HOME/site-latest/* ./
   git add -f .
-  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git commit -m "Website for $TRAVIS_TAG"
   git push -fq origin gh-pages > /dev/null
 
-  echo -e "Published Javadoc to gh-pages.\n"  
+  echo -e "Published website to gh-pages.\n"  
 fi
 
