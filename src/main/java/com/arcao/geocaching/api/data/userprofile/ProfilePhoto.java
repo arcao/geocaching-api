5a709d1187864d1e57ec234e7705d96fe049a865
Arcao <arcao@arcao.com>
2018-04-17 06:40:58 +0200

    Fixed NPE in parsing ProfilePhoto

--- src/main/java/com/arcao/geocaching/api/data/userprofile/ProfilePhoto.java
+++ src/main/java/com/arcao/geocaching/api/data/userprofile/ProfilePhoto.java
@@ -2,18 +2,24 @@ package com.arcao.geocaching.api.data.userprofile;
 
 import com.google.auto.value.AutoValue;
 
+import org.jetbrains.annotations.Nullable;
+
 import java.io.Serializable;
 
 @AutoValue
 public abstract class ProfilePhoto implements Serializable {
     private static final long serialVersionUID = 5557754921065357998L;
 
+    @Nullable
     public abstract String photoDescription();
 
+    @Nullable
     public abstract String photoFilename();
 
+    @Nullable
     public abstract String photoName();
 
+    @Nullable
     public abstract String photoUrl();
 
     public static Builder builder() {
@@ -22,13 +28,13 @@ public abstract class ProfilePhoto implements Serializable {
 
     @AutoValue.Builder
     public abstract static class Builder {
-        public abstract Builder photoDescription(String photoDescription);
+        public abstract Builder photoDescription(@Nullable String photoDescription);
 
-        public abstract Builder photoFilename(String photoFilename);
+        public abstract Builder photoFilename(@Nullable String photoFilename);
 
-        public abstract Builder photoName(String photoName);
+        public abstract Builder photoName(@Nullable String photoName);
 
-        public abstract Builder photoUrl(String photoUrl);
+        public abstract Builder photoUrl(@Nullable String photoUrl);
 
         public abstract ProfilePhoto build();
     }
